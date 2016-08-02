2f0f193c7374a9c1c295d35eaa169084310af687
neverland <enix@foxmail.com>
2016-08-02 14:58:50 +0800

    修改配置

--- index.js
+++ index.js
@@ -9,16 +9,7 @@
 
 var rem = require('rem-core');
 
-var entry = module.exports = function (content, file, conf) {
+module.exports = function (content, file, conf) {
 
     return rem.apply(null, arguments);
-};
-
-entry.defaultOptions = {
-    rem: 18,
-    min: 3,
-    dpr: 2,
-    exclude: [
-        'background', 'background-size'
-    ]
 };
